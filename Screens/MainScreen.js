b43ccb27680183fd978c6fac37b121923c21d5ee
UmairHussain123 <umair.umair2d@gmail.com>
2022-03-23 17:20:54 +0500

    updated

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,8 +1,52 @@
 import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 import moment from "moment";
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+
 const MainScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [address, setAddress] = useState(null);
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === "android" && !Constants.isDevice) {
+        setErrorMsg(
+          "Oops, this will not work on Snack in an Android emulator. Try it on your device!"
+        );
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      // console.log("location", location);
+      let { coords } = await Location.getCurrentPositionAsync();
+      if (coords) {
+        let { longitude, latitude } = coords;
+
+        let regionName = await Location.reverseGeocodeAsync({
+          longitude,
+          latitude,
+        });
+        setAddress(regionName[0]);
+        console.log("city  name>>>", regionName[0].city, regionName[0].country);
+      }
+      // console.log(coords.city);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
   return (
     <View style={styles.mainView}>
       <ScrollView>
@@ -10,10 +54,12 @@ const MainScreen = () => {
           <Card.Title
             title="Namz Time"
             subtitle={
-              "Pakistan" +
-              " karachi" +
-              "      " +
-              moment(Date.now()).format("YYYY-MMM-DD")
+              address &&
+              address.country +
+                "  " +
+                address.city +
+                "      " +
+                moment(Date.now()).format("YYYY-MMM-DD")
             }
           />
           {/* <Title>Card title</Title> */}
@@ -29,10 +75,12 @@ const MainScreen = () => {
             <Card.Title
               title="Ramadan Timing"
               subtitle={
-                "Pakistan" +
-                " Karachi" +
-                "      " +
-                moment(Date.now()).format("YYYY-MMM-DD")
+                address &&
+                address.country +
+                  "  " +
+                  address.city +
+                  "      " +
+                  moment(Date.now()).format("YYYY-MMM-DD")
               }
             />
             {/* <Title>Card title</Title> */}
